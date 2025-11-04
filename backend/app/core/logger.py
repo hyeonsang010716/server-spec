a544d5ab79b75adafb5ff3cb7fbbddd9e2b9ffd7
hyeonsang010716 <kyr76789@gmail.com>
2025-11-04 12:03:49 +0900

    refactor: 로깅 비동기 IO 처리로 변경

--- backend/app/core/logger.py
+++ backend/app/core/logger.py
@@ -79,6 +79,7 @@ def setup_logging() -> None:
             format="{message}",
             serialize=True,
             level=config.LOG_LEVEL,
+            enqueue=True
         )
     else:
         # 읽기 쉬운 콘솔 포맷
@@ -87,6 +88,7 @@ def setup_logging() -> None:
             format="<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level> | {extra}",
             level=config.LOG_LEVEL,
             colorize=True,
+            enqueue=True
         )
     
     # 파일 출력 설정
@@ -103,6 +105,7 @@ def setup_logging() -> None:
             serialize=True,
             level=config.LOG_LEVEL,
             encoding="utf-8",
+            enqueue=True
         )
     
     # 표준 logging 라이브러리와 통합
